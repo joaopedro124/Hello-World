561514940c4cdcdab1e9283cfc98d4cebc1fc632
s4d <147156965+joaopedro124@users.noreply.github.com>
2024-09-08 00:26:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ pygame.init()
 tela = pygame.display.set_mode((800, 600))
 
 while True:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+            break
     pygame.display.flip()
+
